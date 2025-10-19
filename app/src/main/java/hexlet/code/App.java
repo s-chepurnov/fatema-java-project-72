af70c52529e12d61bc65509275963d851e84ed55
F-Jahura <f_jahura@yahoo.com>
2025-10-19 22:29:36 +0300

    added db in app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -30,7 +30,7 @@ public class App {
     }
 
     private static String getDatabaseUrl() {
-        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
+        return System.getenv().getOrDefault("db", "jdbc:h2:mem:project");
     }
 
     public static Javalin getApp() throws IOException, SQLException {
